37bf85c6aeaf8e15d845ee88f3dad094ec1b7734
Xavier Thomas <xave211@gmail.com>
2020-06-29 21:24:04 +0100

    wip

---
+++ src/mocks.js
@@ -0,0 +1,164 @@
+export const MOCK_ASSUME_ROLE_ARN = 'arn:aws:iam::123456789012:role/FakeRole';
+
+
+export const mockUserParameters = {
+	assumerolename: MOCK_ASSUME_ROLE_ARN,
+	targetname: 'FakePipeline',
+};
+
+// export const mockValidCodePipelineEvent = {
+// 	'CodePipeline.job': {
+// 		data: {
+// 			actionConfiguration: {
+// 				configuration: {
+// 					UserParameters: JSON.stringify(mockUserParameters),
+// 				},
+// 			},
+// 		},
+// 	},
+// };
+
+export const mockInvalidCodePipelineEvent = {
+	'CodePipeline.job': {
+		data: {
+			actionConfiguration: {
+				configuration: {
+					UserParameters: null,
+				},
+			},
+		},
+	},
+};
+
+export const mockCodePipelineEvent = {
+	'CodePipeline.job': {
+		data: {
+			actionConfiguration: {
+				configuration: {
+					UserParameters: JSON.stringify(mockUserParameters),
+				},
+			},
+		},
+		id: '2989f000-275f-4031-82bf-cab460511cc4',
+	},
+};
+
+export const mockCodePipelineContext = {
+	invokeid: '15c14512-62df-4db4-8588-9c786c572a83',
+	fail: jest.fn(),
+	succeed: jest.fn(),
+};
+
+export const mockCredentials = {
+	accessKeyId: 'akid',
+	secretAccessKey: 'secret',
+	sessionToken: 'session',
+};
+
+export const mockLambdaContext = {
+	invokeid: '9a2b02ef-28bc-81ea-48be-f2f26b9ef79b',
+};
+
+export const mockAssumedRoleData = {
+	ResponseMetadata: {
+		RequestId: 'a817893e-9b25-11e8-90e6-6f826b9ef79a',
+	},
+	AssumedRoleUser: {
+		Arn: 'arn:aws:sts::123456789012:assumed-role/FakeRole/PipelineMonitoringLambda',
+		AssumedRoleId: 'ARO123EXAMPLE123:FakeRole',
+	},
+	Credentials: {
+		AccessKeyId: 'AKIAIOSFODNN7EXAMPLE',
+		Expiration: '2018-08-08T17:11:07.000Z',
+		SecretAccessKey: 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYzEXAMPLEKEY',
+		SessionToken:
+			'AQoDYXdzEPT//////////wEXAMPLEtc764bNrC9SAPBSM22wDOk4x4HIZ8j4FZTwdQWLWsKWHGBuFqwAeMicRXmxfpSPfIeoIYRqTflfKD8YUuwthAx7mSEI/qkPpKPi/kMcGdQrmGdeehM4IC1NtBmUpp2wUE8phUZampKsburEDy0KPkyQDYwT7WZ0wq5VSXDvp75YU9HFvlRd8Tx6q6fE8YQcHNVXAkiY9q6d+xo0rKwT38xVqr7ZD0u0iPPkUL64lIZbqBAz+scqKmlzm8FDrypNC9Yjc8fPOLn9FX9KSYvKTr4rvx3iSIlTJabIQwj2ICCR/oLxBA==',
+	},
+	PackedPolicySize: 6,
+};
+
+export const mockCredentialsObject = {
+	expired: false,
+	expireTime: '2018-08-08T17:11:07.000Z',
+	accessKeyId: 'ASIASXBQRWFASXWKV646C',
+	sessionToken:
+		'FQoGZXIvYXdzEMr//////////wEaDGEJJQBmW+i5TjdZXyL8AW1Uk77mRGl95xK9Jj4Y91pasfagwagasfw2WIRwrisNUy+0gvb874062Rml8iz/xKi8OmUraIS4czmHBm7WICHXWTAJaTZmMYZFO3Ha5nf0FEs1i8hzf2bbmfc6tUDK1fnsV2/jD8HNE2yGpNHyXnE8S+LHlvHZ9YmjjprY912CjP4D3/Ik8qN/i3PF6w+0GsDgBhIgPzz1qa01f00anh64MQSZiQtO43VeaVwIDyXca0tgJOin68PaquoqipSPLYmLhn9pai3dl0W3zhjzUGJ59Mq482hNHRtNvH5LTiLA9uwE0m39MhlHGd+iq/Z1KqxkQbPbDEjTl51SibrKzbBQ==',
+	masterCredentials: {
+		expired: false,
+		expireTime: null,
+		accessKeyId: 'ASIASVSNOLSJFWNOMA4PY',
+		sessionToken:
+			'FQoGZXIvYXdzEMr//////////wEaDML+QdwbMzuF6kB2DyLuAfzZUd1flyzt2Wa2r2qrqasfatr2asfsa70RIol8E8+LVU5+fzSiW0yZlL7x9Xel7M6k8Dj4u6Oi42f+xYMIze1lfBO4fAULbbagY+GVhGNP3Ic8mHr7yJEUVzuI8iBuJzatuwLhfGF8lEt+/FaRLX9FSkqQxVVnSB2YQ5OlVHF7zOmaIB1yvySPm2IcQGPijHSYTazJP2lYxAD1EJfdvrJNPpW/K0YiasWeRv2F5cjT/GC3ve1teszNMuBdV2eoYd8wP2IGOYw2fpTmNQVnsXuQYetO1Mon/c0Iomays2wU=',
+		envPrefix: 'AWS',
+	},
+	params: {},
+};
+
+export const mockFailedPipelineResult = {
+	pipelineName: 'infrastructure-lambdas-build-deploy-pipeline',
+	stageStates: [
+		{
+			stageName: 'Source',
+			latestExecution: {
+				status: 'Succeeded',
+			},
+		},
+		{
+			stageName: 'Build',
+			latestExecution: {
+				status: 'Failed',
+			},
+		},
+		{
+			stageName: 'Deploy',
+			latestExecution: {
+				status: 'Succeeded',
+			},
+		},
+	],
+};
+
+export const mockSuccessfulPipelineResult = {
+	pipelineName: 'infrastructure-lambdas-build-deploy-pipeline',
+	stageStates: [
+		{
+			stageName: 'Source',
+			latestExecution: {
+				status: 'Succeeded',
+			},
+		},
+		{
+			stageName: 'Build',
+			latestExecution: {
+				status: 'Succeeded',
+			},
+		},
+		{
+			stageName: 'Deploy',
+			latestExecution: {
+				status: 'Succeeded',
+			},
+		},
+	],
+};
+
+export const mockPendingPipelineresult = {
+	pipelineName: 'infrastructure-lambdas-build-deploy-pipeline',
+	stageStates: [
+		{
+			stageName: 'Source',
+			latestExecution: {
+				status: 'Succeeded',
+			},
+		},
+		{
+			stageName: 'Build',
+			latestExecution: {
+				status: 'InProgress',
+			},
+		},
+		{
+			stageName: 'Deploy',
+		},
+	],
+};
--- src/sts.test.js
+++ src/sts.test.js
@@ -1,57 +1,51 @@
-const AWS = require('aws-sdk-mock');
-const { assumeRole, getCredentials } = require('./sts');
+jest.mock('aws-sdk');
+import { assumeRole, getCredentials } from './sts';
+import { MOCK_ASSUME_ROLE_ARN, mockAssumedRoleData, mockCredentialsObject } from './mocks';
+import AWS from 'aws-sdk';
 
-const mockCredentialsData = {
-	AssumedRoleUser: {
-		Arn: 'arn:aws:sts::123456789012:assumed-role/FakeRole/PipelineMonitoringLambda',
-		AssumedRoleId: 'ARO123EXAMPLE123:FakeRole',
-	},
-	Credentials: {
-		AccessKeyId: 'AKIAIOSFODNN7EXAMPLE',
-		Expiration: 1534156391,
-		SecretAccessKey: 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYzEXAMPLEKEY',
-		SessionToken:
-			'AQoDYXdzEPT//////////wEXAMPLEtc764bNrC9SAPBSM22wDOk4x4HIZ8j4FZTwdQWLWsKWHGBuFqwAeMicRXmxfpSPfIeoIYRqTflfKD8YUuwthAx7mSEI/qkPpKPi/kMcGdQrmGdeehM4IC1NtBmUpp2wUE8phUZampKsburEDy0KPkyQDYwT7WZ0wq5VSXDvp75YU9HFvlRd8Tx6q6fE8YQcHNVXAkiY9q6d+xo0rKwT38xVqr7ZD0u0iPPkUL64lIZbqBAz+scqKmlzm8FDrypNC9Yjc8fPOLn9FX9KSYvKTr4rvx3iSIlTJabIQwj2ICCR/oLxBA==',
-	},
-	PackedPolicySize: 6,
-};
+describe('[sts.js] unit tests', () => {
 
-describe('STS', () => {
-	it('must assume a cross account role', () => {
-		const assumeRoleResultSpy = jest.fn();
-		AWS.mock('STS', 'assumeRole', assumeRoleResultSpy);
-		assumeRole('arn:aws:iam::123456789012:role/FakeRole');
-		expect(assumeRoleResultSpy).toHaveBeenCalledWith(
-			{
-				RoleArn: 'arn:aws:iam::123456789012:role/FakeRole',
-				RoleSessionName: 'PipelineMonitoringLambda',
-			},
-			expect.anything()
-		);
-		AWS.restore('CodePipeline');
+	const mockAssumeRole = jest.fn();
+	const mockGetCredentials = jest.fn();
+
+	beforeEach(() => {
+		jest.restoreAllMocks();
+		AWS.STS.mockImplementation(() => ({
+			assumeRole: mockAssumeRole,
+			credentialsFrom: mockGetCredentials
+		}));
 	});
 
-	it('must get an AWS Credentials Object from the AssumeRole output data', () => {
-		const credentialsFromResultSpy = jest.fn();
-		AWS.mock('STS', 'credentialsFrom', credentialsFromResultSpy);
-		getCredentials(mockCredentialsData);
-		expect(credentialsFromResultSpy).toHaveBeenCalledWith(
-			{
-				AssumedRoleUser: {
-					Arn: 'arn:aws:sts::123456789012:assumed-role/FakeRole/PipelineMonitoringLambda',
-					AssumedRoleId: 'ARO123EXAMPLE123:FakeRole',
-				},
-				Credentials: {
-					AccessKeyId: 'AKIAIOSFODNN7EXAMPLE',
-					Expiration: 1534156391,
-					SecretAccessKey: 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYzEXAMPLEKEY',
-					SessionToken:
-						'AQoDYXdzEPT//////////wEXAMPLEtc764bNrC9SAPBSM22wDOk4x4HIZ8j4FZTwdQWLWsKWHGBuFqwAeMicRXmxfpSPfIeoIYRqTflfKD8YUuwthAx7mSEI/qkPpKPi/kMcGdQrmGdeehM4IC1NtBmUpp2wUE8phUZampKsburEDy0KPkyQDYwT7WZ0wq5VSXDvp75YU9HFvlRd8Tx6q6fE8YQcHNVXAkiY9q6d+xo0rKwT38xVqr7ZD0u0iPPkUL64lIZbqBAz+scqKmlzm8FDrypNC9Yjc8fPOLn9FX9KSYvKTr4rvx3iSIlTJabIQwj2ICCR/oLxBA==',
-				},
-				PackedPolicySize: 6,
-			},
-			expect.anything()
-		);
-		AWS.restore('CodePipeline');
+	afterEach(() => {
+		jest.clearAllMocks();
 	});
+
+	describe('[getCredentials] when a valid assumeRoleData is passed', () => {
+		it('must assume a cross account role', async () => {
+			mockAssumeRole.mockResolvedValue(mockAssumedRoleData);
+			const response = await assumeRole(MOCK_ASSUME_ROLE_ARN);
+			expect(response).toEqual(mockAssumedRoleData);
+
+			//expect(await assumeRole()).toEqual(mockAssumedRoleData);
+			expect(mockAssumeRole).toHaveBeenCalledWith(
+				{
+					RoleArn: MOCK_ASSUME_ROLE_ARN,
+					RoleSessionName: 'PipelineMonitoringLambda',
+				}
+			);
+		});
+
+	});
+
+	
+	describe('[getCredentials] when a valid assumeRoleData is passed', () => {
+
+		it('must get an AWS Credentials Object from the AssumeRole output data', async () => {
+			mockGetCredentials.mockResolvedValue(mockCredentialsObject);
+	 		const response = await getCredentials(mockAssumedRoleData);
+	 		expect(response).toEqual(mockCredentialsObject);
+		});
+
+	});
+
 });
